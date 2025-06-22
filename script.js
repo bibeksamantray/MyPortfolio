adc699288b2282bdf126717125c425ce64207eb3
bibeksamantray <164773866+bibeksamantray@users.noreply.github.com>
2025-06-22 17:42:26 +0530

    Added by upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling on anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
